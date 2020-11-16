aa6ce7b0b73f02028c4ee686186303419d3ee35c
Eli Ribble <eliribble@google.com>
2020-11-16 09:47:45 -0800

    Add initial logic to set up the Python module
    This includes a simple 'Hello World' style entrypoint that doesn't do anything
    yet and the logic to install it into a virtual environment.

---
+++ setup.py
@@ -0,0 +1,15 @@
+"Logic for installing the module."
+import setuptools # type: ignore
+
+setuptools.setup(
+	install_requires=[
+		"precommit-message-preservation==0.6",
+	],
+	extras_require={
+		"develop": [
+			"mypy",
+			"nose2",
+			"pylint"
+		]
+	},
+)
